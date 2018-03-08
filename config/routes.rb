729a2cf0dcaf7238d7f1b60f67c2446c7021244d
Eduardo <eduardo.paramo.l@gmail.com>
2018-03-07 21:31:57 -0600

    Changed routes to GeoIP Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :geo_ips
+  # resources :geo_ips
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'geo_i_ps#index'
+
 end
