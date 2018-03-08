e24b4f616947097c63d3f4aa3b5ea97f209b1d85
Eduardo <eduardo.paramo.l@gmail.com>
2018-03-07 20:19:02 -0600

    rails destroy scaffold Weather

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180307175149) do
-
-  create_table "weathers", force: :cascade do |t|
-    t.string "city"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
